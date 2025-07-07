2b18a5a9f4b64f3d06396b5af64ac3f32c6f6d5b
SujanSapkota0923 <SujanSapkota0923@users.noreply.github.com>
2025-07-07 07:34:03 +0000

    deploy: 7bf9204df9a2271b178cd5b66fff7e29ef689325

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/fellowship_code',
+  plugins: [react()],
+})
+
+// config to export repo on github pages
